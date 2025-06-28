410b7599a9bd499ddecf6c712e04d4baec9b6d02
Luis Starlino <luis.guilherme009@gmail.com>
2025-06-28 09:35:51 -0300

    feat(ddd): apply the pattern to resolve this challenge and project

---
+++ MemorableAI.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Domain.Enums
+{
+    public enum TaskStatus
+    {
+        Pending,
+        InProgress,
+        Completed,
+        Archived,
+    }
+}
