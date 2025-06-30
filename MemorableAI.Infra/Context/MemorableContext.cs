3c91c7040138f6da5196d7935c7f9e273e5c5397
Luis Starlino <luis.guilherme009@gmail.com>
2025-06-30 20:24:41 -0300

    feat(context): Finish the integration using Entity Framework. Now, i gonna create the repositoty functions

--- MemorableAI.Infra/Context/MemorableContext.cs
+++ MemorableAI.Infra/Context/MemorableContext.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MemorableAI.Infra.Context
 {
@@ -11,7 +10,7 @@ namespace MemorableAI.Infra.Context
     {
         public MemorableContext(DbContextOptions<MemorableContext> options) : base(options){}
 
-        public DbSet<Task> Tasks { get; set; }
+        public DbSet<MemorableAI.Domain.Models.Task> Tasks { get; set; }
 
     }
 }
---
+++ MemorableAI.Infra/Context/MemorableContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Infra.Context
+{
+    public class MemorableContextFactory : IDesignTimeDbContextFactory<MemorableContext>
+    {
+        public MemorableContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MemorableContext>();
+
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=memorabledb;Username=admin;Password=admin123");
+
+            return new MemorableContext(optionsBuilder.Options);
+
+        }
+    }
+}
