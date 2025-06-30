41a6797e4650487657c5a4637c5eae1f1945e0a7
Luis Starlino <luis.guilherme009@gmail.com>
2025-06-30 20:13:18 -0300

    feat(context): created the infra project to use Entity Framework

---
+++ MemorableAI.Infra/Context/MemorableContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Infra.Context
+{
+    public class MemorableContext : DbContext
+    {
+        public MemorableContext(DbContextOptions<MemorableContext> options) : base(options){}
+
+        public DbSet<Task> Tasks { get; set; }
+
+    }
+}
---
+++ MemorableAI.Infra/Repository/MemorableRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Infra.Repository
+{
+    public class MemorableRepository
+    {
+    }
+}
