dc61a3681e56c52b8fa02638b6959c986a99b37f
Luis Starlino <luis.guilherme009@gmail.com>
2025-07-08 22:31:14 -0300

    feat(repository): Finished the repository layer. Now, i gonna create the services functions

---
+++ MemorableAI.Domain/Interfaces/IMemorableRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Domain.Interfaces
+{
+    public interface IMemorableRepository
+    {
+        Task<int> AddNewTask(Models.Task newTaskModel);
+        Task<Models.Task?> DeleteTask(int idTask);
+        Task<Models.Task?> UpdateTask(Models.Task updatedTask, int idTask);
+        Task<List<Models.Task>> GetAllTask();
+        Task<Models.Task?> GetUniqueTaskById(int idTask);
+    }
+}
