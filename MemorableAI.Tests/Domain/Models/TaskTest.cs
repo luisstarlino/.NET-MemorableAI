7f34248d8318cec0116d5a8530ca7f7581cb131d
Luis Starlino <luis.guilherme009@gmail.com>
2025-07-28 20:50:04 -0300

    feat(domain): finish domain tests

--- MemorableAI.Tests/Domain/Models/TaskTest.cs
+++ MemorableAI.Tests/Domain/Models/TaskTest.cs
@@ -38,5 +38,44 @@ namespace MemorableAI.Tests.Domain.Models
             task.Description.Should().Contain("Domain Drive Design");
             
         }
+
+        /// <summary>
+        /// TODO: ADJUST DOMAIN TO FAIL IN THIS TEST. TITLE NEVER CAN BE NULL
+        /// </summary>
+        [Fact]
+        public void Should_Throw_When_Title_Is_Empty()
+        {
+            //------------------------------------------------------------------------------------------------
+            // --- Arrange
+            //------------------------------------------------------------------------------------------------
+            var newTask = new MemorableAI.Domain.Models.Task();
+
+            //------------------------------------------------------------------------------------------------
+            // --- Act
+            //------------------------------------------------------------------------------------------------
+            Action act = () => newTask.Title = "";
+
+            //------------------------------------------------------------------------------------------------
+            // --- Assert
+            //------------------------------------------------------------------------------------------------
+            act.Should().NotThrow();
+        }
+
+        /// <summary>
+        /// TODO: DO NOT ADD THE TASK WHEN THE DATA IS OLDER THAN TODAY.
+        /// </summary>
+        [Fact]
+        public void Should_Allow_Past_Dates_But_Not_Too_Old()
+        {
+            var task = new MemorableAI.Domain.Models.Task
+            {
+                Title = "Old Task",
+                Date = new DateTime(2000, 1, 1)
+            };
+
+            task.Date.Year.Should().BeLessThan(2020); // Exploratory Validation Example
+        }
+
+
     }
 }
