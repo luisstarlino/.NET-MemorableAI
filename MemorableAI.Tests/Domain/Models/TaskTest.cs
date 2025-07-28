37ee871e1d0124f831c657b46a4306bbcd2d9db2
Luis Starlino <luis.guilherme009@gmail.com>
2025-07-28 20:37:07 -0300

    feat(domain): start the unit test into the domain layer

---
+++ MemorableAI.Tests/Domain/Models/TaskTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Tests.Domain.Models
+{
+    public class TaskTest
+    {
+        [Fact]
+        public void Should_Create_Task_With_Valid_Properties()
+        {
+            //------------------------------------------------------------------------------------------------
+            // --- Arrange
+            //------------------------------------------------------------------------------------------------
+            var createdAt = DateTime.Now;
+            var createdBy = "xUNIT TESTE";
+
+            //------------------------------------------------------------------------------------------------
+            // --- Act
+            //------------------------------------------------------------------------------------------------
+            var task = new MemorableAI.Domain.Models.Task
+            {
+                Id = 1,
+                CreateBy = createdBy,
+                Title = "Study Unit Tests",
+                Description = "This is a arrange to test properties. Using Domain Drive Design",
+                Date = createdAt
+            };
+
+            //------------------------------------------------------------------------------------------------
+            // --- Assert
+            //------------------------------------------------------------------------------------------------
+            task.Id.Should().Be(1);
+            task.CreateBy.Should().Be(createdBy);
+            task.Description.Should().Contain("Domain Drive Design");
+            
+        }
+    }
+}
---
+++ MemorableAI.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MemorableAI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
