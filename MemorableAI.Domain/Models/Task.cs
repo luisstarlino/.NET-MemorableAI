308a9c3c3b7883998980227ee8a6193c31f814b9
Luis Starlino <luis.guilherme009@gmail.com>
2025-07-29 19:48:31 -0300

    refactor(model): update model to create tests cases

--- MemorableAI.Domain/Models/Task.cs
+++ MemorableAI.Domain/Models/Task.cs
@@ -11,10 +11,32 @@ namespace MemorableAI.Domain.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         public string CreateBy { get; set; }
         public DateTime Date { get; set; }
-        //public EnumStatusTarefa Status { get; set; }
+        
+        protected Task()
+        {
+            // Default constructor for EF Core
+        }
+
+        public Task(string title, string description)
+        {
+            if(string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentException("Title cannot be null or empty.", nameof(title));
+            } 
+            if(string.IsNullOrWhiteSpace(description))
+            {
+                throw new ArgumentException("Description cannot be null or empty.", nameof(description));
+            }
+
+            Title = title;
+            Description = description;
+            CreateBy = "MEMORABLE AI";
+            Date = DateTime.Now;
+        }
     }
 }
