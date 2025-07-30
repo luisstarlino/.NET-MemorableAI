0c9e444ce52768be6f2752b3bc376ddacccbb4c7
Luis Starlino <luis.guilherme009@gmail.com>
2025-07-29 22:18:36 -0300

    feat(context): add the in memory EF to run integration tests

---
+++ MemorableAI.Tests/Infra/UnitTests/Repository/MemorableRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using MemorableAI.Infra.Repository;
+using MemorableAI.Tests.Infra.UnitTests.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Tests.Infra.UnitTests.Repository
+{
+    public class MemorableRepositoryTests
+    {
+        [Fact]
+        public async Task AddNewTask_Should_Add_Successfully()
+        {
+            //------------------------------------------------------------------------------------------------
+            // --- Arrange
+            //------------------------------------------------------------------------------------------------
+            var context = new FakeMemorableDBContext().GetInMemoryDbContext();
+            var titleMoc = "Integration Title Test";
+            var repository = new MemorableRepository(context);
+
+            var newTask = new MemorableAI.Domain.Models.Task(titleMoc, "Test");
+
+            //------------------------------------------------------------------------------------------------
+            // --- Act
+            //------------------------------------------------------------------------------------------------
+            var taskId = await repository.AddNewTask(newTask);
+
+            //------------------------------------------------------------------------------------------------
+            // --- Assert
+            //------------------------------------------------------------------------------------------------
+            var addedTask = await repository.GetUniqueTaskById(taskId);
+            Assert.NotNull(addedTask);
+            Assert.Equal(titleMoc, addedTask.Title);
+        }
+    }
+}
