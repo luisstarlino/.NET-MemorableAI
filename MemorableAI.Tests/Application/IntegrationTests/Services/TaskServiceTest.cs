17bab99f52408f613f83f8c13b91fc5cc25c2fc4
Luis Starlino <luis.guilherme009@gmail.com>
2025-07-31 22:21:37 -0300

    feat(service): add test finish

--- MemorableAI.Tests/Application/IntegrationTests/Services/TaskServiceTest.cs
+++ MemorableAI.Tests/Application/IntegrationTests/Services/TaskServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using MemorableAI.Application.Interfaces;
+﻿using FluentAssertions;
+using MemorableAI.Application.Interfaces;
+using MemorableAI.Application.Models;
 using MemorableAI.Application.Services;
 using MemorableAI.Domain.Interfaces;
 using Moq;
@@ -19,6 +21,30 @@ namespace MemorableAI.Tests.Application.IntegrationTests.Services
         {
             _repositoryMock = new Mock<IMemorableRepository>();
             _service = new TaskService(_repositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task ProcessAndSaveNewTask_Should_Return_Task_When_Success()
+        {
+            //------------------------------------------------------------------------------------------------
+            // --- Arrange
+            //------------------------------------------------------------------------------------------------
+            var request = new TaskRequestModel { Title = "Test", Description = "Some Description" };
+
+            // --- Arrange the repository mock to return '1' as the new task ID when AddNewTask is called with any Task object
+            _repositoryMock.Setup(repo => repo.AddNewTask(It.IsAny<MemorableAI.Domain.Models.Task>())).ReturnsAsync(1);
+
+            //------------------------------------------------------------------------------------------------
+            // --- Act
+            //------------------------------------------------------------------------------------------------
+            var result = await _service.ProcessAndSaveNewTask(request);
+
+            //------------------------------------------------------------------------------------------------
+            // --- Arrange
+            //------------------------------------------------------------------------------------------------
+            result.Should().NotBeNull();
+            result.Title.Should().Be("Test");
+            result.Description.Should().Contain("Description");
         }
     }
 }
