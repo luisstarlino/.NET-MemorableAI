47c28565ddc24813fb310ad085da37ba9e474584
Luis Starlino <luis.guilherme009@gmail.com>
2025-07-31 22:11:27 -0300

    feat(files): start to add architecture to the last tast case

---
+++ MemorableAI.Tests/Application/IntegrationTests/Services/TaskServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using MemorableAI.Application.Interfaces;
+using MemorableAI.Application.Services;
+using MemorableAI.Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemorableAI.Tests.Application.IntegrationTests.Services
+{
+    public class TaskServiceTest
+    {
+        private Mock<IMemorableRepository> _repositoryMock;
+        private readonly TaskService _service;
+
+        public TaskServiceTest()
+        {
+            _repositoryMock = new Mock<IMemorableRepository>();
+            _service = new TaskService(_repositoryMock.Object);
+        }
+    }
+}
